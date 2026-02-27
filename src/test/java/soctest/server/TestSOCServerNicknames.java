22688c552cbeb64ae7fef05d8ddd13a5b813b342
szakal-byt3 <lesakal@protonmail.com>
2026-02-26 21:32:02 -0800

    Add test to check dupe nicknames are rejected

--- src/test/java/soctest/server/TestSOCServerNicknames.java
+++ src/test/java/soctest/server/TestSOCServerNicknames.java
@@ -42,4 +42,42 @@ public class TestSOCServerNicknames {
         assertEquals("Piped nickname rejected", -2, rc);
     }
     
+    /* Nicknames should not be duplicated unless they have reached timeout
+     * The {@link SOCServer#checkNickname()} function should test for this as well
+     */
+    @Test
+    public void testCheckNicknameDuplicateReturnsInUse() throws Exception
+    {
+        String nickname = "Exists";
+
+        SOCServer server = new SOCServer("test", 0, null, null);
+
+        // create local connection for test
+        StringConnection serverSide = new StringConnection();
+        StringConnection clientSide = new StringConnection(serverSide);
+        serverSide.setAccepted();
+
+        serverSide.setData(nickname);     // set nickname on the connection
+        server.addConnection(serverSide); // add to server's connection list
+
+        // new connection for attempted duplicate nickname
+        StringConnection newServerSide = new StringConnection();
+        StringConnection newClientSide = new StringConnection(newServerSide);
+        newServerSide.setAccepted();
+
+        Method method = SOCServer.class.getDeclaredMethod(
+            "checkNickname",
+            String.class,
+            Connection.class,
+            boolean.class,
+            boolean.class
+        );
+        method.setAccessible(true);
+
+        int rc = (Integer) method.invoke(server, nickname, newClientSide, false, false);
+
+        // checkNickname() returns 0 if okay, -1 if taking over a nickname, or -2 if bad
+        // either -1 or -2 is fine, just not 0
+        assertTrue("Duplicate nickname rejected or timed out", rc != 0);
+    }
 }
