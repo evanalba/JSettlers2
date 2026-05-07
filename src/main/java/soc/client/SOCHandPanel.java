ceb166eca34b78aa9e05a0a1f8610bca919e42f6
szakal-byt3 <lesakal@protonmail.com>
2026-05-07 09:12:27 -0700

    Remove accidental import

--- src/main/java/soc/client/SOCHandPanel.java
+++ src/main/java/soc/client/SOCHandPanel.java
@@ -45,7 +45,6 @@ import java.awt.FontMetrics;
 import java.awt.Insets;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
-import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
